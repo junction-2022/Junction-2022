136d6d003f9cf5b74188082ea5d6dca4ed3c99e5
“”Nikolai <semin.nikolai@bk.ru>
2022-11-04 22:20:30 +0200

    Added models

--- EMOlater/backend/models.py
+++ EMOlater/backend/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class SurveyRepsponse(models.Model):
+    body = models.CharField(max_length=50)
+    user_id = models.IntegerField()
+    timestamp = models.DateField()
+    family = models.BooleanField()
+    partner = models.BooleanField()
+    friends = models.BooleanField()
+    colleagues = models.BooleanField()
+    weather = models.IntegerField()
+    sleep = models.FloatField()
+    work = models.FloatField()
+    sports = models.IntegerField()
+    hobbies = models.IntegerField()
+    social = models.IntegerField()
+    health = models.IntegerField()
+    mood = models.IntegerField()
+
+class User(models.Model)
+    user_ID = models.IntegerField()
+    user_name = models.CharField(max_length=50)
+    user_password = models.CharField(max_length=50)
