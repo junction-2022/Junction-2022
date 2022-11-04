d0111fa20d6c1faf0a9b8476a94dd5a0d60c2c0d
Xu Zehui <zehuixu@whu.edu.cn>
2022-11-04 23:08:47 +0200

    database config

--- EMOlater/EMOlater/settings.py
+++ EMOlater/EMOlater/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = "EMOlater.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": 'backend',
+        "HOST": '10.100.27.14',
+        "USER": 'root',
+        "PASSWORD": 'password',
     }
 }
 
