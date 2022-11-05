00a7c40887725eeacdbac2dcfdde470c51e1d770
Xu Zehui <zehuixu@whu.edu.cn>
2022-11-05 09:05:13 +0200

    restapi initial implementation

--- EMOlater/backend/urls.py
+++ EMOlater/backend/urls.py
@@ -1,26 +1,16 @@
 # backend/urls.py
 from django.contrib import admin
 from django.urls import path, include
-from .models import SurveyRepsponse
-from rest_framework import routers, serializers, viewsets
-
-
-class SurveyResponseSerializers(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'is_staff']
-
-
-class SurveyResponseViewSet(viewsets.ModelViewSet):
-    queryset = SurveyRepsponse.objects.all()
-    serializer_class = SurveyResponseSerializers
+from rest_framework import routers
+from .views import *
 
 router = routers.DefaultRouter()
-router.register(r'survey_responses', SurveyResponseViewSet)
+router.register(r'all_users', UserViewSet)
+router.register(r'all_survey_repsponses', SurveyRepsponseViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('backend.urls')),
     path('', include(router.urls)),
-    path('api-survey/', include('rest_framework.urls', namespace='rest_framework'))
+    path('user/', user),
+    path('survey/', survey)
 ]
