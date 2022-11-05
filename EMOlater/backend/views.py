7aa9c9997e0073b1ac6c9de0bd5ba2117bf67100
Xu Zehui <zehuixu@whu.edu.cn>
2022-11-05 13:27:17 +0200

    handle GET request to /survey

--- EMOlater/backend/views.py
+++ EMOlater/backend/views.py
@@ -38,5 +38,13 @@ def survey(request):
             return Response(data=serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     elif request.method == 'GET':
-        serializer = SurveyRepsponseSerializer(SurveyRepsponse.objects.all().last())
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        date = request.query_params.get('date', None)
+        username = request.query_params.get('username', None)
+        if date:
+            responses = SurveyRepsponse.objects.filter(timestamp__lte=date)
+            return Response(SurveyRepsponseSerializer(list(responses), many=True).data, status=status.HTTP_200_OK)
+        elif username:
+            responses = SurveyRepsponse.objects.filter(user_name=username)
+            return Response(SurveyRepsponseSerializer(list(responses), many=True).data, status=status.HTTP_200_OK)
+        else:
+            return Response(SurveyRepsponseSerializer(SurveyRepsponse.objects.all().last()).data)
