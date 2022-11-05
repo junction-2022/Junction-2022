d19794e182e8992d1532a900e77de97e5c33e9c5
Xu Zehui <zehuixu@whu.edu.cn>
2022-11-05 23:15:16 +0200

    change DateField to DateTimeField

--- EMOlater/backend/models.py
+++ EMOlater/backend/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-import datetime
+from datetime import datetime
 
 class SurveyRepsponse(models.Model):
     name = models.CharField(max_length=20)
-    timestamp = models.DateField(default=datetime.date.today())
+    timestamp = models.DateTimeField(default=datetime.now())
     family = models.BooleanField(default=False)
     partner = models.BooleanField(default=False)
     friends = models.BooleanField(default=False)
